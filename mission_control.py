2ee8b48105f6b351c8a7058ad13cf792300db3bd
pFerreras0203 <pedro.ferreras8@gmail.com>
2026-05-26 22:08:07 -0300

    matriz dos dados e areas monitoradas

--- mission_control.py
+++ mission_control.py
@@ -1,1 +1,19 @@
-print('hello world')
+NOME_MISSAO = "Missao"
+NOME_EQUIPE = "Pedro's"
+
+dados_missao = [
+    [22, 95, 91, 98, 95],
+    [26, 83, 76, 95, 88],
+    [32, 61, 54, 90, 68],
+    [37, 44, 35, 85, 52],
+    [41, 25, 17, 76, 32],
+    [35, 52, 29, 80, 47],
+]
+
+areas_monitoradas = [
+    "Temperatura interna",
+    "Comunicacao com a base",
+    "Sistema de energia",
+    "Suporte de oxigenio",
+    "Estabilidade operacional"
+]
